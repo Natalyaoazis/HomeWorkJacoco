f3dc00513cac6d2e1797e42023f3527b379c17ab
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-09-05 15:23:18 +0300

    test changed

--- src/test/java/ru/netology/stats/HomeWorkJacocoTest.java
+++ src/test/java/ru/netology/stats/HomeWorkJacocoTest.java
@@ -8,8 +8,8 @@ class HomeWorkJacocoTest {
     @Test
     void findMax() {
         HomeWorkJacoco service = new HomeWorkJacoco();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 29};
+        long expected = 29;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
